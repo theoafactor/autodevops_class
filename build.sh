20b6b8374fe8086d04111fef7ee5d7b319cd57ea
Olu Adeyemo <theoafactor@gmail.com>
2025-03-18 14:50:18 +0100

    First commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DockerfileName="Dockerfile"
+
+if [[ -f $DockerfileName ]]
+then
+    echo "Dockerfile exists already"
+    echo "FROM nginx:alpine" > $DockerfileName
+    echo "RUN ls" >> $DockerfileName 
+else 
+    ## file does not exist, create it
+    echo "File does not exist..."
+    echo "Creating it ..."
+    sleep 3
+    touch $DockerfileName
+    echo "$DockerfileName file created ..."
+    
+    echo "FROM nginx:alpine" >> $DockerfileName
+    echo "RUN ls" >> $DockerfileName    
+fi
+
